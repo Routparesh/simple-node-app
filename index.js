dcfa19ebe988077ecd28ae7cf37bb8c8c4db92d5
Paresh Kumar Rout <routpareshkumar737@gmail.com>
2025-07-19 22:55:51 +0530

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+	res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+	console.log(`Server running at http://localhost:${PORT}`);
 });
